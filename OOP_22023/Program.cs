1ed59cb85f029fbd56fb8bc6ff3ec9e8a6844724
João Monge <joao.monge13@gmail.com>
2022-11-09 10:09:37 +0000

    Adicionada a classe Carro

--- OOP_22023/Program.cs
+++ OOP_22023/Program.cs
@@ -12,9 +12,16 @@ Fruta banana = new Fruta("banana", "amarelo", 1.4);
 Fruta morango = new Fruta("morango", "vermelho", 3.5);
 Fruta maca = new Fruta("maca", "verde", 1.2);
 
+Carro mercedes = new Carro("Mercedes", "C220", "AA-00-BB", "LigeiroPassageiros");
+Carro peugeot = new Carro("Peugeot", "206", "FF-13-45", "LigeirosPassageiros");
+
+String nome = "Joao";
+
 app.MapGet("/", () => "Hello 22023");
 app.MapGet("/ufcd", () => "3935");
-app.MapGet("/morango", () => morango.cor);
+app.MapGet("/morango", () => morango.calcularPreco(0.200));
+app.MapGet("/banana", () => banana.toString());
+app.MapGet("/end", () => nome.EndsWith("a"));
 
 app.Run();
 
@@ -26,10 +33,40 @@ class Fruta
     public string cor;
     public double precoKg;
 
-    public Fruta(string nome,string cor,double precoKg)
+    public Fruta(string nome, string cor, double precoKg)
     {
         this.nome = nome;
         this.cor = cor;
         this.precoKg = precoKg;
     }
+
+    public String toString()
+    {
+        String texto = $"Nome: {nome} Cor: {cor} Preço: {precoKg}";
+        return texto;
+    }
+
+    public String calcularPreco(double peso) {
+        double resultado = peso*precoKg;
+        String texto = $"O custo de {peso} kg de {nome} foi {resultado}";
+        return texto;
+    }
+}
+
+
+class Carro {
+    public String marca;
+    public String modelo;
+    public String matricula;
+    public String categoriaVeiculo;
+
+    //Constructor da classe
+    public Carro(String marca, String modelo, String matricula,String categoriaVeiculo)
+    {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.matricula = matricula;
+        this.categoriaVeiculo = categoriaVeiculo;
+    }
+
 }
